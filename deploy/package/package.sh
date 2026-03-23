5d44e8964a3b4e5764c3e47457859bc9b94fac43
sudebaker <sudebaker@protonmail.com>
2026-03-23 08:10:37 +0100

    fix(package): rewrite compose volume paths for target deployment directory layout

--- deploy/package/package.sh
+++ deploy/package/package.sh
@@ -143,8 +143,9 @@ tar -czf "$DIST_DIR/models.tar.gz" "$MODELS_DIR"
 # Step 5: Copy compose files + .env.example
 # ---------------------------------------------------------------------------
 log "Copying config files ..."
-cp "$COMPOSE_BASE"            "$DIST_DIR/config/docker-compose.yml"
-cp "$COMPOSE_GPU"             "$DIST_DIR/config/docker-compose.gpu.yml"
+sed 's|../../models|../models|g' "$COMPOSE_BASE" > "$DIST_DIR/config/docker-compose.yml"
+sed 's|../../models|../models|g' "$COMPOSE_GPU"  > "$DIST_DIR/config/docker-compose.gpu.yml"
+log "  Rewrote ../../models -> ../models for target deployment layout"
 if [[ -f ".env.example" ]]; then
   cp ".env.example" "$DIST_DIR/config/.env.example"
 else
